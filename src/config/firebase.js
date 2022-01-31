060923535f8fb3094336699dc1c2dfbcb7d570d4
matias <matifandy@gmail.com>
2022-01-30 22:10:44 -0300

    add firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+//   apiKey: "AIzaSyCxXRtk4S0qhjtWMNmL31RkcPBxvtsugf4",
+//   authDomain: "dev-dashboard-9eff0.firebaseapp.com",
+//   projectId: "dev-dashboard-9eff0",
+//   storageBucket: "dev-dashboard-9eff0.appspot.com",
+//   messagingSenderId: "508965551787",
+//   appId: "1:508965551787:web:94b81878585fe4dceac1ee"
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+export const auth = app.auth()
+const app = initializeApp(firebaseConfig);
