2c469c8c824a00a5959afb59c748a4df7b9a1705
matias <matifandy@gmail.com>
2022-02-27 15:04:26 -0300

    auth styles mobile

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -1,37 +1,133 @@
-import React, { useState } from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import React, { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+type UserSubmitForm = {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  acceptTerms: boolean;
+};
 
 export interface ILoginPageProps {}
 
 const Login: React.FunctionComponent<ILoginPageProps> = (props) => {
-    const auth = getAuth();
-    const navigate = useNavigate();
-    const [authing, setAuthing] = useState(false);
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [authing, setAuthing] = useState(false);
 
-    const signInWithGoogle = async () => {
-        setAuthing(true);
+  const signInWithGoogle = async () => {
+    setAuthing(true);
 
-        signInWithPopup(auth, new GoogleAuthProvider())
-            .then((response) => {
-                console.log(response.user.uid);
-                window.localStorage.setItem("token", response.user.uid);
-                navigate('/');
-            })
-            .catch((error) => {
-                console.log(error);
-                setAuthing(false);
-            });
-    };
+    signInWithPopup(auth, new GoogleAuthProvider())
+      .then((response) => {
+        console.log(response.user.uid);
+        window.localStorage.setItem("token", response.user.uid);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthing(false);
+      });
+  };
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+    acceptTerms: Yup.bool().oneOf([true], "Accept Terms is required"),
+  });
 
-    return (
-        <div>
-            <p>Login Page</p>
-            <button onClick={() => signInWithGoogle()} disabled={authing}>
-                Sign in with Google
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data: UserSubmitForm) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
+  return (
+    <div className="register">
+      <h1>Login to Dev-Dashboard</h1>
+      <button
+        className="register__google"
+        onClick={() => signInWithGoogle()}
+        disabled={authing}
+      >
+        <img
+          src="http://assets.stickpng.com/images/5847f9cbcef1014c0b5e48c8.png"
+          alt=""
+        />
+        Sign in with Google
+      </button>
+      <div className="registerForm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+        
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="text"
+              {...register("email")}
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.email?.message}</div>
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              {...register("password")}
+              className={`form-control ${errors.password ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.password?.message}</div>
+          </div>
+        
+
+          <div className="form-check">
+            <input
+              type="checkbox"
+              {...register("acceptTerms")}
+              className={`form-check-input ${
+                errors.acceptTerms ? "is-invalid" : ""
+              }`}
+            />
+            <label htmlFor="acceptTerms" className="form-check-label">
+              I have read and agree to the Terms
+            </label>
+            <div className="invalid-feedback">
+              {errors.acceptTerms?.message}
+            </div>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Register
             </button>
-        </div>
-    );
+            <button
+              type="button"
+              onClick={() => reset()}
+              className="btn btn-warning float-right"
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/auth/register/register.tsx
+++ src/pages/auth/register/register.tsx
@@ -1,8 +1,163 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+type UserSubmitForm = {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  acceptTerms: boolean;
+};
 
 const Register = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [authing, setAuthing] = useState(false);
+
+  const signInWithGoogle = async () => {
+    setAuthing(true);
+
+    signInWithPopup(auth, new GoogleAuthProvider())
+      .then((response) => {
+        console.log(response.user.uid);
+        window.localStorage.setItem("token", response.user.uid);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthing(false);
+      });
+  };
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string().required("Full name is required"),
+    username: Yup.string()
+      .required("Username is required")
+      .min(6, "Username must be at least 6 characters")
+      .max(20, "Username must not exceed 20 characters"),
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+    confirmPassword: Yup.string()
+      .required("Confirm Password is required")
+      .oneOf([Yup.ref("password"), null], "Confirm Password does not match"),
+    acceptTerms: Yup.bool().oneOf([true], "Accept Terms is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data: UserSubmitForm) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <div>
-      <h1>register</h1>
+    <div className="register">
+      <h1>Register to Dev-Dashboard</h1>
+      <button className="register__google" onClick={() => signInWithGoogle()} disabled={authing}>
+        <img src="http://assets.stickpng.com/images/5847f9cbcef1014c0b5e48c8.png" alt="" />
+        Sign in with Google
+      </button>
+      <div className="registerForm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-field-group">
+            <div className="form-group">
+            <label>Full Name</label>
+            <input
+              type="text"
+              {...register("fullname")}
+              className={`form-control ${errors.fullname ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.fullname?.message}</div>
+          </div>
+
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              {...register("username")}
+              className={`form-control ${errors.username ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.username?.message}</div>
+          </div>
+          </div>
+        
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="text"
+              {...register("email")}
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.email?.message}</div>
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              {...register("password")}
+              className={`form-control ${errors.password ? "is-invalid" : ""}`}
+            />
+            <div className="invalid-feedback">{errors.password?.message}</div>
+          </div>
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              {...register("confirmPassword")}
+              className={`form-control ${
+                errors.confirmPassword ? "is-invalid" : ""
+              }`}
+            />
+            <div className="invalid-feedback">
+              {errors.confirmPassword?.message}
+            </div>
+          </div>
+
+          <div className="form-check">
+            <input
+              type="checkbox"
+              {...register("acceptTerms")}
+              className={`form-check-input ${
+                errors.acceptTerms ? "is-invalid" : ""
+              }`}
+            />
+            <label htmlFor="acceptTerms" className="form-check-label">
+              I have read and agree to the Terms
+            </label>
+            <div className="invalid-feedback">
+              {errors.acceptTerms?.message}
+            </div>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+            <button
+              type="button"
+              onClick={() => reset()}
+              className="btn btn-warning float-right"
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
