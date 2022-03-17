fb49d1c5115ae69411c3cc950c3f5df29a751853
matias <matifandy@gmail.com>
2022-03-17 20:00:44 -0300

    structure home and calendar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import Layout from "../../layout";
+import MyCalendar from "./components/Calendar";
 
 const Home = () => {
   return (
@@ -6,12 +7,10 @@ const Home = () => {
       <div className="home">
         {/* deveria de ser un componente unico con sus funcionalidades */}
         <div className="home__projects">
-        <div className="title">
+          <div className="title">
             <h2>Projects</h2>
-            <div className="dots">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                <path d="M64 360C94.93 360 120 385.1 120 416C120 446.9 94.93 472 64 472C33.07 472 8 446.9 8 416C8 385.1 33.07 360 64 360zM64 200C94.93 200 120 225.1 120 256C120 286.9 94.93 312 64 312C33.07 312 8 286.9 8 256C8 225.1 33.07 200 64 200zM64 152C33.07 152 8 126.9 8 96C8 65.07 33.07 40 64 40C94.93 40 120 65.07 120 96C120 126.9 94.93 152 64 152z" />
-              </svg>
+            <div className="all">
+              <a href="/projects">See All</a>
             </div>
           </div>
           <h3>Project 1</h3>
@@ -23,14 +22,33 @@ const Home = () => {
         <div className="home__learning">
           <div className="title">
             <h2>Learning</h2>
-            <div className="dots">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                <path d="M64 360C94.93 360 120 385.1 120 416C120 446.9 94.93 472 64 472C33.07 472 8 446.9 8 416C8 385.1 33.07 360 64 360zM64 200C94.93 200 120 225.1 120 256C120 286.9 94.93 312 64 312C33.07 312 8 286.9 8 256C8 225.1 33.07 200 64 200zM64 152C33.07 152 8 126.9 8 96C8 65.07 33.07 40 64 40C94.93 40 120 65.07 120 96C120 126.9 94.93 152 64 152z" />
-              </svg>
+            <div className="all">
+              <a href="/learning">See All</a>
             </div>
           </div>
           <h3>HTML</h3>
           <h3>CSS</h3>
+        </div>
+        <div className="home__socialmedia">
+          <div className="title">
+            <h2>Social Media</h2>
+            <div className="all">
+              <a href="/social-media">See All</a>
+            </div>
+          </div>
+          <h3>TWITTER</h3>
+          <h3>GITHUB</h3>
+        </div>
+        <div className="home__calendar">
+          <div className="title">
+            <h2>Your Calendar</h2>
+            <div className="all">
+              <a href="/calendar">See All</a>
+            </div>
+          </div>
+          <div className="calendar">
+            <MyCalendar />
+          </div>
         </div>
       </div>
     </Layout>
